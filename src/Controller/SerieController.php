75fe91f30695e38c0c138ded7cb561f56e283d21
ffotre <ffotre@campus-eni.fr>
2024-02-08 16:16:42 +0100

    Using ParamConverter

--- src/Controller/SerieController.php
+++ src/Controller/SerieController.php
@@ -2,7 +2,9 @@
 
 namespace App\Controller;
 
+use App\Entity\Serie;
 use App\Repository\SerieRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -11,15 +13,33 @@ use Symfony\Component\Routing\Annotation\Route;
 class SerieController extends AbstractController
 {
     #[Route('/detail/{id}', name: '_detail', requirements: ['id' => '\d+'])]
-    public function detail(int $id, SerieRepository $serieRepository): Response
+    public function detail(Serie $serie): Response
     {
-        $serie = $serieRepository->find($id);
-
         return $this->render('serie/detail.html.twig', [
             'serie' => $serie
         ]);
     }
 
+    #[Route('/update/{id}', name: '_update', requirements: ['id' => '\d+'])]
+    public function update(Serie $serie, EntityManagerInterface $em): Response
+    {
+        $serie->setName('Friends');
+
+        $em->persist($serie);
+        $em->flush();
+
+        return new Response ('C\'est updated !');
+    }
+
+    #[Route('/delete/{id}', name: '_delete', requirements: ['id' => '\d+'])]
+    public function delete(serie $serie, EntityManagerInterface $em): Response
+    {
+
+        $em->remove($serie);
+        $em->flush();
+
+        return $this->redirectToRoute('app_home');
+    }
 
 
 
