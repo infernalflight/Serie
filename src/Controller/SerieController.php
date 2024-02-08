775aba0c952080ee21a41d07a4cf00c75eff3322
ffotre <ffotre@campus-eni.fr>
2024-02-08 16:25:44 +0100

    New templating

--- src/Controller/SerieController.php
+++ src/Controller/SerieController.php
@@ -3,7 +3,6 @@
 namespace App\Controller;
 
 use App\Entity\Serie;
-use App\Repository\SerieRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
