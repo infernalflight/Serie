8c419bf1b1f0839c310c00b595744b6d4a533cb0
ffotre <ffotre@campus-eni.fr>
2024-02-15 11:07:01 +0100

    Forgot Password

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -27,32 +27,9 @@ class RegistrationFormType extends AbstractType
                     ]),
                 ],
             ])
-            ->add('plainPassword', RepeatedType::class, [
-                'type' => PasswordType::class,
-                'options' => [
-                    'attr' => [
-                        'autocomplete' => 'new-password',
-                    ],
-                ],
-                'first_options' =>  [
-                    'constraints' => [
-                        new NotBlank([
-                            'message' => 'Please enter a password',
-                        ]),
-                        new Length([
-                            'min' => 6,
-                            'minMessage' => 'Your password should be at least {{ limit }} characters',
-                            // max length allowed by Symfony for security reasons
-                            'max' => 4096,
-                        ]),
-                    ],
-                    'label' => 'Choose a Password'
-                ],
-                'second_options' => [
-                    'label' => 'Confirm your password',
-                ],
-                'invalid_message' => 'The password fields must match',
-                'mapped' => false,
+            ->add('plainPassword', ChangePasswordFormType::class, [
+                'inherit_data' => true,
+                'label' => false,
             ])
         ;
     }
