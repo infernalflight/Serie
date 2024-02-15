ae1320c8b437a077d7c8d36c72363d908deff2bf
ffotre <ffotre@campus-eni.fr>
2024-02-15 12:23:51 +0100

    Service Creation and Integration

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -4,18 +4,18 @@ namespace App\Controller;
 
 use App\Entity\User;
 use App\Form\RegistrationFormType;
+use App\Helper\Sender;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Contracts\Translation\TranslatorInterface;
 
 class RegistrationController extends AbstractController
 {
     #[Route('/register', name: 'app_register')]
-    public function register(Request $request, UserPasswordHasherInterface $userPasswordHasher, EntityManagerInterface $entityManager): Response
+    public function register(Request $request, UserPasswordHasherInterface $userPasswordHasher, EntityManagerInterface $entityManager, Sender $sender): Response
     {
         $user = new User();
         $form = $this->createForm(RegistrationFormType::class, $user);
@@ -34,7 +34,8 @@ class RegistrationController extends AbstractController
 
             $entityManager->persist($user);
             $entityManager->flush();
-            // do anything else you need here, like send an email
+
+            $sender->sendEmail('New User', 'Un nouvel utilisateur : ' . $user->getEmail() . ' vient de s\'inscrire.', 'admin@serie.com');
 
             return $this->redirectToRoute('app_login');
         }
---
+++ src/Helper/Sender.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Helper;
+
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Mime\Email;
+
+class Sender
+{
+    public function __construct(private MailerInterface $mailer) {}
+
+
+    public function sendEmail(string $subject, string $text, string $dest) {
+        $email = new Email();
+        $email->subject($subject)
+            ->text($text)
+            ->from('no-reply@serie.com')
+            ->to($dest);
+
+        $this->mailer->send($email);
+    }
+}
