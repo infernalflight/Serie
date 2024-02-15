d785f5044c3fbcfca053d21d924acd338f67a3dd
ffotre <ffotre@campus-eni.fr>
2024-02-15 12:07:01 +0100

    Fix Form Imbrication

--- src/Form/ChangePasswordFormType.php
+++ src/Form/ChangePasswordFormType.php
@@ -3,19 +3,15 @@
 namespace App\Form;
 
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\PasswordType;
-use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
-use Symfony\Component\Validator\Constraints\Length;
-use Symfony\Component\Validator\Constraints\NotBlank;
 
 class ChangePasswordFormType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('plainPassword', ChangePasswordFormType::class, [
+            ->add('plainPassword', ChangePasswordType::class, [
                 'inherit_data' => true,
                 'label' => false,
             ]);
