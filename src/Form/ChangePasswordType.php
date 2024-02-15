52ce29f1abdf2a48d0953d04c97f7c2f1be66532
ffotre <ffotre@campus-eni.fr>
2024-02-15 13:51:52 +0100

    Fix imbrication

--- src/Form/ChangePasswordType.php
+++ src/Form/ChangePasswordType.php
@@ -8,6 +8,7 @@ use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\Validator\Constraints\Length;
 use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class ChangePasswordType extends AbstractType
 {
