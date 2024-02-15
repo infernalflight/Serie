7153b04639693c0dd53747d8cc7eea3dc38aa7d7
ffotre <ffotre@campus-eni.fr>
2024-02-15 12:04:47 +0100

    Fix Form Imbrication

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -5,13 +5,9 @@ namespace App\Form;
 use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
-use Symfony\Component\Form\Extension\Core\Type\PasswordType;
-use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Validator\Constraints\IsTrue;
-use Symfony\Component\Validator\Constraints\Length;
-use Symfony\Component\Validator\Constraints\NotBlank;
 
 class RegistrationFormType extends AbstractType
 {
@@ -27,7 +23,7 @@ class RegistrationFormType extends AbstractType
                     ]),
                 ],
             ])
-            ->add('plainPassword', ChangePasswordFormType::class, [
+            ->add('plainPassword', ChangePasswordType::class, [
                 'inherit_data' => true,
                 'label' => false,
             ])
